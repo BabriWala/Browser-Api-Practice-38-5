7cedba1dcf1526068fa8e769e12cbf1c39d45c6d
BabriWala <hanzalafarhann@gmail.com>
2022-09-07 12:32:45 +0600

    2nd Question is done

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const delay = setTimeout(()=>{
     console.log("Second")
 }, 3500);
 console.log("third");
+
+// take a number from users, using prompt and add 250 to that number
+document.getElementById('giveNumber').addEventListener('click', ()=>{
+    const number = prompt('Give a Number');
+    // console.log(typeof number)
+    const convertingNumber = parseInt(number);
+    const newNumber = convertingNumber + 250;
+    alert(newNumber);
+})
+
